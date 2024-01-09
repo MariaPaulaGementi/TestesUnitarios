c51535993275b46af30a5ab867d1eb430b5a6f49
MariaPaulaGementi <maria_paula0809@outlook.com.br>
2024-01-09 11:20:58 -0300

    tratamento de exceções 1

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -18,10 +19,12 @@ public class LocacaoServiceTest {
 	
 	@Rule
 	public ErrorCollector error = new ErrorCollector();
-		
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
 	
 	@Test
-	public void testeLocacao() {
+	public void testeLocacao() throws Exception {
 		//iniciando teste do metodo de alugarFilme
 		//cenario
 		LocacaoService servico = new LocacaoService();
@@ -42,7 +45,52 @@ public class LocacaoServiceTest {
 		//utilizando o rule
 		error.checkThat(locacao.getValor(), CoreMatchers.is(5.0));
 		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), CoreMatchers.is(true));
-		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),  CoreMatchers.is(false));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),  CoreMatchers.is(true));
 		
 	}
+	
+	//tratamento de exceção "elegante"
+	@Test(expected=Exception.class) //o teste já espera uma exceção
+	public void testeLocacao_FilmesSemEstoque() throws Exception {
+		LocacaoService servico = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("filme" ,0, 5.0);
+		
+		servico.alugarFilme(usuario, filme);
+		
+	}
+	
+	//tratamento de exceção "robusta"
+	@Test 
+	public void testeLocacao_FilmesSemEstoque2() {
+		LocacaoService servico = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("filme" ,0, 5.0);
+		
+		try {
+			servico.alugarFilme(usuario, filme);
+			Assert.fail("Deveria ter lançado uma exceção");
+		} catch (Exception e) {
+			Assert.assertThat(e.getMessage(), CoreMatchers.is("Filme sem estoque"));
+		}
+		
+	}
+	
+	//tratamento de exceção "nova"
+	//declara a espera da exceção antes da ação
+		@Test
+		public void testeLocacao_FilmesSemEstoque3() throws Exception {
+			//cenário
+			LocacaoService servico = new LocacaoService();
+			Usuario usuario = new Usuario("Usuario 1");
+			Filme filme = new Filme("filme" ,0, 5.0);
+			
+			//exceção
+			exception.expect(Exception.class);
+			exception.expectMessage("Filme sem estoque");
+			
+			//ação
+			servico.alugarFilme(usuario, filme);
+
+		}
 }
